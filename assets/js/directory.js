c051b68c841b693af42af42c10bdbd238c3e7c7f
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-04-24 22:31:17 -0500

    Add social indicators to main table view.

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -5,13 +5,6 @@ $(document).ready(function() {
   	dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>>" +
       "<'row'<'col-sm-12'tr>>" +
       "<'row'<'col-sm-5'i><'col-sm-7'p>>",
-    pageLength: 50,
-    columnDefs: [
-      {
-        targets: [1],
-        orderable: false,
-        searchable: false
-      }
-    ]
+    pageLength: 50
   });
 });
