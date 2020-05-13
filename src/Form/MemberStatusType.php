eab091aa317ed60e5367856e0907bd0191ab184d
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-05-12 19:43:58 -0500

    Don't allow code to change once created

--- src/Form/MemberStatusType.php
+++ src/Form/MemberStatusType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\MemberStatus;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MemberStatusType extends AbstractType
@@ -12,9 +14,17 @@ class MemberStatusType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
             ->add('label')
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $memberStatus = $event->getData();
+            $form = $event->getForm();
+            if (!$memberStatus || null === $memberStatus->getId()) {
+                $form->add('code');
+            }
+        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
