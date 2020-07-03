3d97e2c3f9da384f0afd16963f457cf2fd761761
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-07-02 23:39:17 -0500

    Add monthly donation graphs
    Also refactors the stat boxes underneath them.

--- src/Repository/DonationRepository.php
+++ src/Repository/DonationRepository.php
@@ -48,20 +48,44 @@ class DonationRepository extends ServiceEntityRepository
     public function getTotalDonations()
     {
         return $this->createQueryBuilder('d')
-            ->select('sum(d.amount) AS totalAmount, sum(d.processingFee) AS totalProcessingFee, sum(d.netAmount) AS totalNetAmount, d.currency')
+            ->select('COUNT(d) AS totalDonations, COUNT(DISTINCT d.member) AS totalDonors, SUM(d.amount) AS totalAmount, SUM(d.processingFee) AS totalProcessingFee, SUM(d.netAmount) AS totalNetAmount, d.currency')
             ->groupBy('d.currency')
             ->getQuery()
             ->getResult();
     }
 
+    public function getTotalDonationsByMonth()
+    {
+        return $this->createQueryBuilder('d')
+            ->select('DATE_FORMAT(d.receivedAt, \'%Y-%m-01\') AS aggregatedDate, COUNT(d) AS totalDonations, COUNT(DISTINCT d.member) AS totalDonors, SUM(d.amount) AS totalAmount, SUM(d.processingFee) AS totalProcessingFee, SUM(d.netAmount) AS totalNetAmount, d.currency')
+            ->groupBy('d.currency', 'aggregatedDate')
+            ->orderBy('aggregatedDate', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
     public function getTotalDonationsForMember(Member $member)
     {
         return $this->createQueryBuilder('d')
-            ->select('sum(d.amount) AS totalAmount, sum(d.processingFee) AS totalProcessingFee, sum(d.netAmount) AS totalNetAmount, d.currency')
+            ->select('COUNT(d) AS totalDonations, SUM(d.amount) AS totalAmount, SUM(d.processingFee) AS totalProcessingFee, SUM(d.netAmount) AS totalNetAmount, d.currency')
             ->join('d.member', 'm')
             ->where('d.member = :member')
             ->setParameter('member', $member)
             ->groupBy('d.currency')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getTotalDonationsByMonthForMember(Member $member)
+    {
+        return $this->createQueryBuilder('d')
+            ->select('DATE_FORMAT(d.receivedAt, \'%Y-%m-01\') AS aggregatedDate, COUNT(d) AS totalDonations, COUNT(DISTINCT d.member) AS totalDonors, SUM(d.amount) AS totalAmount, SUM(d.processingFee) AS totalProcessingFee, SUM(d.netAmount) AS totalNetAmount, d.currency')
+            ->groupBy('d.currency', 'aggregatedDate')
+            ->orderBy('aggregatedDate', 'ASC')
+            ->join('d.member', 'm')
+            ->where('d.member = :member')
+            ->setParameter('member', $member)
             ->getQuery()
             ->getResult();
     }
