10195c8b91b575b8905442165dc5728c4b2627b7
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-10-22 22:16:13 -0500

    Switch Loggable string columns to 191
    Related: https://github.com/Atlantic18/DoctrineExtensions/issues/1941

---
+++ migrations/Version20201023030317.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201023030317 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Updates for Gedmo Loggable';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $extLogEntriesTable = $schema->getTable('ext_log_entries');
+        $extLogEntriesTable->changeColumn('object_class', ['length' => 191, 'nullable' => false]);
+        $extLogEntriesTable->changeColumn('username', ['length' => 191, 'nullable' => true]);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $extLogEntriesTable = $schema->getTable('ext_log_entries');
+        $extLogEntriesTable->changeColumn('object_class', ['length' => 255, 'nullable' => false]);
+        $extLogEntriesTable->changeColumn('username', ['length' => 255, 'nullable' => true]);
+    }
+}
