2afc1e8af5fb5b85415496023c8b50682148ba85
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-05-09 13:45:08 -0500

    Fixup migration

--- migrations/Version20210508034754.php
+++ migrations/Version20210508034754.php
@@ -7,26 +7,36 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20210508034754 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add PasswordResetRequest table.';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $resetPasswordRequestTable = $schema->createTable('reset_password_request');
+        $resetPasswordRequestTable->addColumn('id', 'integer', ['autoincrement' => true, 'notnull' => true]);
+        $resetPasswordRequestTable->addColumn('user_id', 'integer', ['notnull' => true]);
+        $resetPasswordRequestTable->addColumn('selector', 'string', ['length' => 20, 'notnull' => true]);
+        $resetPasswordRequestTable->addColumn('hashed_token', 'string', ['length' => 100, 'notnull' => true]);
+        $resetPasswordRequestTable->addColumn('requested_at', 'datetime', ['notnull' => true, 'comment' => '(DC2Type:datetime_immutable)']);
+        $resetPasswordRequestTable->addColumn('expires_at', 'datetime', ['notnull' => true, 'comment' => '(DC2Type:datetime_immutable)']);
+        $resetPasswordRequestTable->addIndex(['user_id'], 'IDX_7CE748AA76ED395');
+        $resetPasswordRequestTable->setPrimaryKey(['id']);
+
+        $userTable = $schema->getTable('user');
+        $resetPasswordRequestTable->addForeignKeyConstraint($userTable, ['user_id'], ['id'], [], 'FK_7CE748AA76ED395');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE reset_password_request');
+        $schema->dropTable('reset_password_request');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
     }
 }
