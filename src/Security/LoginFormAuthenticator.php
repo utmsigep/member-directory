ce5e4fb474b801465c0271cfb6f486b5781d512b
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-05-11 21:19:24 -0500

    Don't tell user that email not found.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -68,7 +68,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Invalid credentials.');
         }
 
         return $user;
