8e343bdb97f3f56dae1f52ee83599db7c8386abf
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-06-12 20:02:05 -0500

    Add setup script

--- src/Command/UserAddCommand.php
+++ src/Command/UserAddCommand.php
@@ -33,7 +33,7 @@ class UserAddCommand extends Command
     {
         $this
             ->setDescription('Adds an administrative user to the system.')
-            ->addArgument('email', InputArgument::REQUIRED, 'Email address of the user')
+            ->addArgument('email', InputArgument::OPTIONAL, 'Email address of the user')
             ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'Password for user')
             ->addOption('roles', 'r', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'One or more roles', ['ROLE_ADMIN'])
         ;
@@ -52,6 +52,15 @@ class UserAddCommand extends Command
             $valid_roles = array_values(User::USER_ROLES);
             if (!in_array($role, $valid_roles)) {
                 $io->error(sprintf("Invalid role: %s\n\nValid: %s", $role, implode(', ', $valid_roles)));
+                return Command::FAILURE;
+            }
+        }
+
+        if (!$email) {
+            $question = new Question('Enter your email address:');
+            $email = $helper->ask($input, $output, $question);
+            if (!$email) {
+                $io->error('You must provide an email address.');
                 return Command::FAILURE;
             }
         }
