f0ada312e06b379fcaac03eb9b244d2eeef1706d
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-07-06 16:28:42 -0500

    Improve UI for attendees

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\Member;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -28,19 +31,31 @@ class EventType extends AbstractType
             ])
             ->add('location')
             ->add('description')
-            ->add('attendees', null, [
-                'placeholder' => '',
-                'query_builder' => function(EntityRepository $er) {
-                    return $er->createQueryBuilder('m')
-                        ->join('m.status', 's')
-                        ->addOrderBy('s.label', 'ASC')
-                        ->addOrderBy('m.lastName', 'ASC')
-                        ->addOrderBy('m.preferredName', 'ASC')
-                    ;
-                },
-                'group_by' => function($choice, $key, $value) {
-                    return $choice->getStatus()->getLabel();
-                }
+            ->add('attendees', CollectionType::class, [
+                'label' => false,
+                'entry_type' => EntityType::class,
+                'entry_options' => [
+                    'class' => Member::class,
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('m')
+                            ->join('m.status', 's')
+                            ->addOrderBy('s.label', 'ASC')
+                            ->addOrderBy('m.lastName', 'ASC')
+                            ->addOrderBy('m.preferredName', 'ASC')
+                        ;
+                    },
+                    'group_by' => function($choice, $key, $value) {
+                        return $choice->getStatus()->getLabel();
+                    },
+                    'attr' => [
+                        'class' => 'selectpicker',
+                        'data-live-search' => true,
+                        'title' => 'Search for Member ...'
+                    ]
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
             ])
         ;
     }
