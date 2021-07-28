ba103204a4d5b0ab80d71c5025d035196954c52e
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-07-27 20:44:01 -0500

    Order by last login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,7 +25,7 @@ class UserController extends AbstractController
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findBy([], ['email' => 'ASC']),
+            'users' => $userRepository->findBy([], ['lastLogin' => 'DESC']),
         ]);
     }
 
