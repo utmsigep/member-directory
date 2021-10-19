e0613c72b00e96903525c4f43d10d82002ccbbf9
Stephen Yeargin <stephen.yeargin@gmail.com>
2021-10-19 15:55:06 -0500

    Remove invalid migration config

--- migrations/Version20201023030317.php
+++ migrations/Version20201023030317.php
@@ -17,15 +17,15 @@ final class Version20201023030317 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $extLogEntriesTable = $schema->getTable('ext_log_entries');
-        $extLogEntriesTable->changeColumn('object_class', ['length' => 191, 'nullable' => false]);
-        $extLogEntriesTable->changeColumn('username', ['length' => 191, 'nullable' => true]);
+        $extLogEntriesTable->changeColumn('object_class', ['length' => 191]);
+        $extLogEntriesTable->changeColumn('username', ['length' => 191]);
     }
 
     public function down(Schema $schema): void
     {
         $extLogEntriesTable = $schema->getTable('ext_log_entries');
-        $extLogEntriesTable->changeColumn('object_class', ['length' => 255, 'nullable' => false]);
-        $extLogEntriesTable->changeColumn('username', ['length' => 255, 'nullable' => true]);
+        $extLogEntriesTable->changeColumn('object_class', ['length' => 255]);
+        $extLogEntriesTable->changeColumn('username', ['length' => 255]);
     }
 
     public function isTransactional(): bool
